4dea98ec14d199dede415925d77d33a65d478b8f
mbazan16 <mariabazan@gmail.com>
2025-04-08 12:26:34 +0200

    Octavo commit, Modificar pais

--- src/main/java/com/proyectoClase/springHR/controllers/PaisController.java
+++ src/main/java/com/proyectoClase/springHR/controllers/PaisController.java
@@ -4,7 +4,6 @@ import com.proyectoClase.springHR.admin.ServComunAdmin;
 import com.proyectoClase.springHR.admin.ServPaisAdmin;
 import com.proyectoClase.springHR.entities.Pais;
 import com.proyectoClase.springHR.entities.Region;
-import com.proyectoClase.springHR.repositories.RegionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -49,9 +48,7 @@ public class PaisController {
 
     @GetMapping("/nuevo")
     public String irFormularioNuevoPais(Model model){
-        List<Region> regiones = servicioComun.listaRegiones();
-
-        model.addAttribute("regiones",regiones);
+        listaRegiones(model);
         return "t_nuevo_pais";
 
     }
@@ -62,11 +59,39 @@ public class PaisController {
                             @RequestParam Integer idRegion,Model model){
 
        Pais pais= servicio.savePais(id,nombrePais,idRegion);
-
        model.addAttribute("pais",pais);
 
        return "t_pais";
 
+    }
+
+    @GetMapping("/modificar/{id}")
+    public String irFormularioModificarPais(@PathVariable String id, Model model){
+        Pais pais = servicio.getPais(id);
+
+        model.addAttribute("pais",pais);
+
+        listaRegiones(model);
+        return "t_modificar_pais";
+
+    }
+
+
+    @PostMapping("/modificar")
+    public String modificarPais(@ModelAttribute Pais pais, Model model){
+
+        Pais paisAux= servicio.savePais(pais);
+
+        model.addAttribute("pais",paisAux);
+
+        return "t_pais";
+
+    }
+
+
+    private void listaRegiones(Model model) {
+        List<Region> regiones = servicioComun.listaRegiones();
+        model.addAttribute("regiones",regiones);
     }
 
 }
